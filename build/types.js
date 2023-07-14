1bd1b2bc3e4a51135988207d3e431438e57d9ab3
1r2a3s4u5l <abdujamolovrasuljonpeser@gmail.com>
2023-07-14 08:14:08 +0500

    second commit

--- build/types.js
+++ build/types.js
@@ -124,5 +124,208 @@ const myCat = {
     color: "Qora",
     age: 12,
 };
+const rect1 = {
+    lineWidth: 2,
+    size: {
+        width: 30,
+        height: 20,
+    },
+};
+rect1.color = "blue";
+rect1.size.width = 50;
+const rect2 = {
+    lineWidth: 2,
+    size: {
+        width: 30,
+        height: 20,
+    },
+    getArea() {
+        return this.size.width * this.size.height;
+    },
+};
+console.log(rect2.getArea());
+const css = {
+    border: "3px solid red",
+    borderRedius: "5px",
+    borderGround: "Red",
+    color: "Yellow",
+    line: 2,
+};
+function connect(connection) {
+    if (connection == "default") {
+        console.log(connection);
+    }
+    else {
+        console.log(connection);
+    }
+}
+connect({ host: "localhost", port: 233 });
+connect("default");
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 7] = "Down";
+    Direction1[Direction1["Left"] = 8] = "Left";
+    Direction1[Direction1["Right"] = 9] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+Direction1.Right;
+Direction2.Left;
+var Users;
+(function (Users) {
+    Users[Users["user"] = 0] = "user";
+    Users[Users["admin"] = 1] = "admin";
+    Users[Users["superuser"] = 2] = "superuser";
+})(Users || (Users = {}));
+console.log(Users.admin);
+console.log(Users[1]);
+function position(a, b) {
+    if (!a && !b) {
+        console.log("Salim");
+        return { x: undefined, y: undefined };
+    }
+    if (a && !b) {
+        return { x: a, y: undefined };
+    }
+    return { x: a, y: b };
+}
+position();
+function myLog(mes) {
+    console.log(mes);
+    return mes;
+}
+myLog(3);
+myLog("satr");
+function myLog1(mes) {
+    console.log(mes);
+    return mes;
+}
+myLog1("satr");
+myLog1(23);
+myLog1(true);
+function myLog2(mes, arr) {
+    console.log(mes);
+    return arr;
+}
+myLog2(true, [12, 121]);
+console.log(myLog2("Salom", [12, 121]));
+function myLog3(password, arr) {
+    if (password.length < 10)
+        console.log(password.length);
+    return arr;
+}
+myLog3("Salomlar", [1, 2]);
+myLog3([1, 2, "salom"], [1, 2]);
+const Numbers = [1, 2, 3];
+const Numbers1 = [1, 2, 3];
+const Strings = ["1", "2", "3"];
+function reverse(array) {
+    return array.reverse();
+}
+console.log(reverse(Numbers));
+console.log(reverse(Numbers1));
+console.log(reverse(Strings));
+const promise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("OK");
+    }, 2000);
+});
+promise.then((data) => {
+    console.log(data.toLowerCase());
+});
+function mergeObj(obj1, obj2) {
+    return Object.assign({}, obj1, obj2);
+}
+const mergeObj1 = mergeObj({ user: "admin" }, { password: "qwerty" });
+const mergeObj2 = mergeObj({ type: "rectangle" }, { area: 100 });
+const mergeObj3 = mergeObj({ model: "BMW" }, { speed: 280 });
+console.log(mergeObj1.user);
+console.log(mergeObj3.speed);
+let s1 = "Salom";
+let s2;
+let a5 = "lat";
+let b5 = "lon";
+let key;
+key = "lat";
+key = "lon";
+let user1 = "name";
+let user2 = {
+    name: "Admin",
+    email: "admin@mail.uz",
+    password: "12345",
+};
+const person = {
+    name: "Salim",
+    age: 33,
+};
+const car = {
+    model: "Hundai",
+    marka: "Solaris",
+    color: "white",
+};
+console.log(getObjValue(person, "name"));
+console.log(getObjValue(person, "age"));
+console.log(getObjValue(car, "model"));
+console.log(getObjValue(car, "color"));
+function getObjValue(obj, key) {
+    return obj[key];
+}
+function log(a) {
+    return a.toLowerCase();
+}
+const big = BigInt(1000);
+console.log(big);
+const sym1 = Symbol("ID");
+const sym2 = Symbol("ID");
+console.log(sym1);
+console.log(sym2);
+console.log(sym1 == sym2);
+class Millat {
+    constructor(name, tili, _dini, _age) {
+        this.name = name;
+        this.tili = tili;
+        this._dini = _dini;
+        this._age = _age;
+    }
+    set age(newAge) {
+        this._age = newAge;
+    }
+    get age() {
+        return this._age;
+    }
+    set dini(religious) {
+        this._dini = religious;
+    }
+    get dini() {
+        return this._dini;
+    }
+    about() {
+        return `Millati: ${this.name}, tili: ${this.tili}`;
+    }
+}
+class Uzbek extends Millat {
+    constructor(name, age, tili, dini, history) {
+        super(name, age, tili, dini);
+        this.history = history;
+    }
+    setLang(lang) {
+        this.tili = lang;
+    }
+    about() {
+        return `${super.about()}, dini: ${this.dini}, tarixi: ${this.history}`;
+    }
+}
+const uzbek = new Uzbek("Uzbek", 23, "uzbek", "Islom", "Buyuk");
+uzbek.setLang("Uzbek tili");
+uzbek.dini = "Islom dini";
+console.log(uzbek.dini);
+uzbek.age = 44;
+console.log(uzbek.about());
 export {};
 //# sourceMappingURL=types.js.map
--- src/types.ts
+++ src/types.ts
@@ -209,3 +209,374 @@ const myCat: Cat = {
   age: 12,
 };
 
+interface IRect {
+  readonly lineWidth: number;
+  color?: string;
+  size: {
+    width: number;
+    height: number;
+  };
+}
+const rect1: IRect = {
+  lineWidth: 2,
+  size: {
+    width: 30,
+    height: 20,
+  },
+};
+rect1.color = "blue";
+rect1.size.width = 50;
+
+interface IRectArea extends IRect {
+  getArea: () => number;
+}
+
+const rect2: IRectArea = {
+  lineWidth: 2,
+  size: {
+    width: 30,
+    height: 20,
+  },
+  getArea(): number {
+    return this.size.width * this.size.height;
+  },
+};
+
+console.log(rect2.getArea());
+
+interface IStyle {
+  [key: string]: string | number;
+}
+
+const css: IStyle = {
+  border: "3px solid red",
+  borderRedius: "5px",
+  borderGround: "Red",
+  color: "Yellow",
+  line: 2,
+};
+
+interface IConnection {
+  host: string;
+  port: number;
+}
+
+function connect(connection: IConnection | "default") {
+  if (connection == "default") {
+    console.log(connection);
+  } else {
+    console.log(connection);
+  }
+}
+
+connect({ host: "localhost", port: 233 });
+connect("default");
+
+// ENUM
+enum Direction1 {
+  Up,
+  Down = 7,
+  Left,
+  Right,
+}
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+Direction1.Right;
+Direction2.Left;
+
+enum Users {
+  user,
+  admin,
+  superuser,
+}
+
+console.log(Users.admin);
+console.log(Users[1]);
+
+// ===============function============
+interface IPosition {
+  x: number;
+  y: number;
+}
+function position(): IPosition;
+function position(a: number): IPosition;
+function position(a: number, b: number): IPosition;
+
+function position(a?: number, b?: number) {
+  if (!a && !b) {
+    console.log("Salim");
+    return { x: undefined, y: undefined };
+  }
+  if (a && !b) {
+    return { x: a, y: undefined };
+  }
+  return { x: a, y: b };
+}
+position();
+
+// ==============generic==========
+function myLog(mes: string | number): string | number {
+  console.log(mes);
+  return mes;
+}
+
+myLog(3);
+myLog("satr");
+// myLog(true)
+
+function myLog1<T>(mes: T): T {
+  console.log(mes);
+  return mes;
+}
+myLog1<string>("satr");
+myLog1<number>(23);
+myLog1<boolean>(true);
+// ---------------------
+function myLog2<P, K>(mes: P, arr: K[]): K[] {
+  console.log(mes);
+  return arr;
+}
+
+myLog2(true, [12, 121]);
+console.log(myLog2<string, number>("Salom", [12, 121]));
+// ========Amalda=======
+interface ILength {
+  length: number;
+}
+function myLog3<T extends ILength, K>(password: T, arr: K[]): K[] {
+  if (password.length < 10) console.log(password.length);
+  return arr;
+}
+myLog3<string, number>("Salomlar", [1, 2]);
+// myLog3(true,[1,2])
+
+myLog3([1, 2, "salom"], [1, 2]);
+// myLog3(2,[1,2])
+
+// ==========Amalda=========
+const Numbers: Array<number> = [1, 2, 3];
+const Numbers1: number[] = [1, 2, 3];
+const Strings: Array<string> = ["1", "2", "3"];
+
+function reverse<T>(array: T[]): T[] {
+  return array.reverse();
+}
+
+console.log(reverse(Numbers));
+console.log(reverse(Numbers1));
+console.log(reverse(Strings));
+
+// ========Amalda===========
+const promise = new Promise<string>((resolve) => {
+  setTimeout(() => {
+    resolve("OK");
+  }, 2000);
+});
+promise.then((data) => {
+  console.log(data.toLowerCase());
+});
+// =====Amalda=========
+// function mergeObj(obj1:object,obj2:object){
+//   return Object.assign({},obj1,obj2)
+// }
+// function mergeObj<T, K>(obj1:T,obj2:K){
+//   return Object.assign({},obj1,obj2)
+// }
+function mergeObj<T extends object, K extends object>(obj1: T, obj2: K) {
+  return Object.assign({}, obj1, obj2);
+}
+
+const mergeObj1 = mergeObj({ user: "admin" }, { password: "qwerty" });
+const mergeObj2 = mergeObj({ type: "rectangle" }, { area: 100 });
+const mergeObj3 = mergeObj({ model: "BMW" }, { speed: 280 });
+
+console.log(mergeObj1.user);
+console.log(mergeObj3.speed);
+
+// ========typeof=======
+let s1 = "Salom";
+
+let s2: typeof s1;
+
+// ========keyof=====
+type Coord = {
+  lat: number;
+  lon: number;
+};
+
+type P = keyof Coord;
+
+let a5: P = "lat";
+let b5: P = "lon";
+// -----------
+interface ICoord1 {
+  lat: number;
+  lon: number;
+}
+type P1 = keyof ICoord;
+let key: P1;
+key = "lat";
+key = "lon";
+
+// =========generic keyof amalda =====
+type User = {
+  _id: number;
+  name: string;
+  email: string;
+  password?: string;
+  createdAt: Date;
+};
+
+type UserNoMeta1 = Exclude<keyof User, "_id" | "createdAt">;
+type UserNoMeta2 = Pick<User, "name" | "email" | "password">;
+
+let user1: UserNoMeta1 = "name";
+let user2: UserNoMeta2 = {
+  name: "Admin",
+  email: "admin@mail.uz",
+  password: "12345",
+};
+
+// =========generic keyof amalda =====
+const person = {
+  name: "Salim",
+  age: 33,
+};
+const car = {
+  model: "Hundai",
+  marka: "Solaris",
+  color: "white",
+};
+console.log(getObjValue(person, "name"));
+console.log(getObjValue(person, "age"));
+console.log(getObjValue(car, "model"));
+console.log(getObjValue(car, "color"));
+
+function getObjValue<O extends object, K extends keyof O>(obj: O, key: K) {
+  return obj[key];
+}
+
+// ===========
+function log(a: string | null) {
+  // if(a!==null){
+  //   return a.toLowerCase()
+  // }
+  // return a
+  return a!.toLowerCase(); //aniq string bo'lsa
+}
+// console.log(log(null));
+// ===========
+const big: bigint = BigInt(1000);
+
+console.log(big);
+
+const sym1: symbol = Symbol("ID");
+const sym2: symbol = Symbol("ID");
+
+console.log(sym1);
+console.log(sym2);
+console.log(sym1 == sym2);
+
+// =================CLASS===============
+// class Bags {
+//   readonly name: string;
+//   color: string;
+//   readonly material: string;
+//   price: number;
+
+//   constructor(name: string, color: string, material: string, price: number) {
+//     this.name = name;
+//     this.color = color;
+//     this.material = material;
+//     this.price = price;
+//   }
+//   info() {
+//     return `${this.name} - ${this.price}`;
+//   }
+// }
+
+// const bag1 = new Bags("Dracon", "metallic", "dracon-teri", 7000);
+
+// console.log(bag1.info());
+
+// // ------------
+// class NewStyleBags {
+//   color: string;
+//   price: number;
+
+//   constructor(readonly name: string, color: string, readonly material: string, price: number) {
+//     this.color = color;
+//     this.material = material;
+//   }
+//   info() {
+//     return `${this.name} - ${this.price}`;
+//   }
+// }
+// ----------
+// class NewStyleBags {
+//   constructor(
+//     readonly name: string,
+//     public color: string,
+//     readonly material: string,
+//     public price: number
+//   ) {}
+//   info() {
+//     return `${this.name} - ${this.price}`;
+//   }
+// }
+
+// =======================
+class Millat{
+  constructor(
+    public name:string,
+    protected tili:string,
+    private _dini:string,
+    private _age:number
+  ) {}
+  set age(newAge:number){
+    this._age=newAge
+  }
+  get age(){
+    return this._age
+  }
+  set dini(religious:string){
+    this._dini = religious
+  }
+  get dini(){
+    return this._dini
+  }
+  about(){
+    return `Millati: ${this.name}, tili: ${this.tili}`
+  }
+}
+class Uzbek extends Millat {
+  public history: string;
+
+  constructor(name: string, age: number, tili: string, dini: string, history: string) {
+    super(name, age, tili, dini);
+    this.history = history;
+  }
+
+  public setLang(lang: string) {
+    this.tili = lang;
+  }
+
+  override about() {
+    return `${super.about()}, dini: ${this.dini}, tarixi: ${this.history}`;
+  }
+}
+
+
+const uzbek = new Uzbek("Uzbek",23,"uzbek","Islom","Buyuk")
+uzbek.setLang("Uzbek tili")
+uzbek.dini = "Islom dini"
+console.log(uzbek.dini);
+uzbek.age = 44
+
+
+console.log(uzbek.about());
